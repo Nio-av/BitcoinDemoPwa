e0255d3b807d456e8674e7507ce7d1b6801608cb
Nicolas T <Nio-av@users.noreply.github.com>
2020-06-29 23:07:25 +0200

    output currencys

--- bitcoinApp/src/components/CurrencySelect.tsx
+++ bitcoinApp/src/components/CurrencySelect.tsx
@@ -1,7 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
+import { IonItem, IonLabel, IonSelect, IonSelectOption } from '@ionic/react';
  
 const API = 'https://api.blockchain.info/';
 const DEFAULT_QUERY = 'ticker';
+
  
 class CurrencySelect extends Component {
   constructor(props : any) {
@@ -19,18 +21,41 @@ class CurrencySelect extends Component {
       .then(data => this.setState( data ));
   }
  
-
-  
   render() {
-    const currencys  = this.state;
+    const currencys : any = this.state;
 
     console.log("Data:")
     console.log(currencys);
+
+    for(let cur in currencys){
+      console.log(cur);
+      console.log(currencys[cur].buy)
+    }
+
+    var listCurrencys = (function () {
+      var entitys = [];
+      for (let Currency in currencys) {
+          entitys.push(<IonSelectOption value={Currency}>{Currency}</IonSelectOption>);
+      }
+  
+      return entitys;
+    }());
+  
+
+    
+    
  
     return (
-      <ul>
+      <IonItem>
+          <IonLabel>Currency:</IonLabel>
+          <IonSelect interface="popover" value='BitcoinPrice.Currency' onIonChange={e => "setCurrency('e.detail.value')"}>
+          
+          {listCurrencys}
+          
+          </IonSelect>
+      </IonItem>
         
-      </ul>
+        
     );
   }
   //*/
