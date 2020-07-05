401f2e18ca7ce161ac45728adc1dc4fed3fc181c
Nicolas T <Nio-av@users.noreply.github.com>
2020-07-05 23:22:18 +0200

    include iOS Install Banner

--- bitcoinApp/src/App.tsx
+++ bitcoinApp/src/App.tsx
@@ -27,6 +27,7 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import IosInstallBanner from './components/IosInstallBanner';
 
 const App: React.FC = () => {
 
@@ -45,6 +46,7 @@ const App: React.FC = () => {
           </IonRouterOutlet>
         </IonSplitPane>
       </IonReactRouter>
+      <IosInstallBanner />
     </IonApp>
   );
 };
---
+++ bitcoinApp/src/components/IosInstallBanner.tsx
@@ -0,0 +1,65 @@
+import { starOutline, star, construct, options } from 'ionicons/icons';
+import { share, add } from 'ionicons/icons';
+import React, { useState } from 'react';
+import { IonToast, IonButton, IonContent, IonIcon } from '@ionic/react';
+
+
+
+function bannerInSession(setBannerInSession : any = false){
+    //check weather InstallPropt has allready been shown in this session
+    if( ( (sessionStorage.getItem('isBannerShown') == (false || null) ) ) && ( setBannerInSession) == true ) {
+        sessionStorage.setItem('isBannerShown', setBannerInSession);
+    }
+    return sessionStorage.getItem('isBannerShown');
+}
+
+
+
+const IosInstallBanner : React.FC = ({  } )=> {
+
+    const [showIosToast, setShowIosToast] = useState( bannerInSession() );
+
+    function showIosInstall (){
+    
+        
+        const isIos = () => {
+            return /iphone|ipad|ipod/.test(window.navigator.userAgent.toLowerCase());
+        };
+        const isInStandaloneMode = () => ('standalone' in (window as any).navigator) && ((window as any).navigator.standalone);
+            console.log("bannerInSession()! " + !bannerInSession() );
+            if( isIos() && !isInStandaloneMode() && !bannerInSession() ){
+                return <IonToast
+                    isOpen={!false}
+                    //onDidDismiss={() => setShowToast2(false)}
+                    message={'To install the app, tap "Share" icon below and select "Add to Home Screen".'}
+                    position="bottom"
+                    onDidDismiss={() => ( bannerInSession(true) ) }
+                    mode="ios"
+                    buttons={[
+                    {
+                        side: 'start',
+                        icon: add,
+                    },
+                    {
+                      text: 'Ok',
+                      role: 'cancel',
+                      handler: () => {
+                        console.log('Cancel clicked');
+                      }
+                    }
+                    ]}
+                />
+            }
+        }
+
+
+    
+    return (
+        <>
+            {showIosInstall()}
+        </>
+    )
+};
+
+
+export default IosInstallBanner;
