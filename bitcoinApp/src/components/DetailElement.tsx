f6c63a25f15d0d76fbc6148e5cbd598608470f81
Nicolas T <Nio-av@users.noreply.github.com>
2020-07-06 22:08:57 +0200

    Transform to class

--- bitcoinApp/src/components/DetailElement.tsx
+++ bitcoinApp/src/components/DetailElement.tsx
@@ -1,17 +1,18 @@
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonSelectOption, IonItem, IonLabel, IonSelect, IonBadge } from '@ionic/react';
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import { useParams } from 'react-router';
 import ExploreContainer from './ExploreContainer';
 
 //const BitcoinPrice = require('https://blockchain.info/ticker');
 
-
+const API = 'https://api.blockchain.info/';
+const DEFAULT_QUERY = 'q/';
 
 interface ContainerProps {
-  metric: string;
+  metricValue: string;
 }
 
-var metric : string;
+var metricValue: string;
 
 /*
 
@@ -34,35 +35,35 @@ fetch('https://api.blockchain.info/ticker')
 
   */
   
+  
 
-const DetailElement : React.FC<ContainerProps> = ( metric ) => {
+class DetailElement extends Component <{metric : string  }, { metricValue: string}> {
+  constructor(props : any, metricValue : string) {
+    super(props);
 
-  //const { metric } = useParams<{ metric: string; }>();
+    
+    this.state = {
+      metricValue : ""
+    };
+    
+  }
 
-  /*
-  console.log(BitcoinPrice);
-  var listCurrency = (function () {
-    var entitys : any = [];
-    for (let Currency in BitcoinPrice) {
-      console.log('Currency here' + Currency);
-      entitys.push(<IonSelectOption value={Currency}>{Currency}</IonSelectOption>);
-    }
 
-    return entitys;
-  }());
-  */
+  render() {
+    const metric = this.props.metric;
+ 
+    return (
+      <IonItem>
+        <IonLabel>{metric}</IonLabel>
+        <IonBadge slot="end">22</IonBadge>
+      </IonItem>
+        
+        
+    );
+  }
+}
 
-  const [ setCurrency ]= useState<any>('EUR');
 
-  console.log(metric.metric);
 
-  return (
-    <IonItem>
-      <IonLabel>{metric.metric}</IonLabel>
-      <IonBadge slot="end">22</IonBadge>
-    </IonItem>
-
-  );
-};
 
 export default DetailElement;
